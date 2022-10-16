a653f027db94caf7b5dbc720f042ea9d7d4ab22b
Eloisa Amorin <eloisa.amorin@keyrus.com.br>
2022-10-15 23:16:43 -0300

    Git init

--- src/Teste.js
+++ src/Teste.js
@@ -1,16 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FormLogin from './Forms';
 
 function App() {
     return (
         <div className="App">
             <body className="App-body">
-                <img src={logo} className="App-logo" alt="logo" />
-
-                <div className="App-formlogin">
-                    <FormLogin />
-                </div>
+            <img src={logo} className="App-logo" alt="logo" />
             </body>
         </div>
     );
